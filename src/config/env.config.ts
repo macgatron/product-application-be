6f01ab2fcae8cf0fd9e8413df6fa5dc1b1b55c42
Budi <budi.webdev@gmail.com>
2024-09-22 18:17:14 +0700

    initial commit

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -6,6 +6,13 @@ interface IConfig {
         host: string,
         port: number,
     },
+    database: {
+        driver: string,
+        host: string,
+        db: string,
+        username: string,
+        password: string,
+    },
     panelo: {
         host: string,
     }
@@ -15,6 +22,13 @@ const config: IConfig = {
     app: {
         host: process.env.HOST || '127.0.0.1',
         port: Number(process.env.PORT || 3000),
+    },
+    database: {
+        driver: process.env.DB_DRIVER || 'mysql',
+        host: process.env.DB_HOST || '127.0.0.1',
+        db: process.env.DB_DATABASE || 'test_db',
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
     },
     panelo: {
         host: process.env.PANELO_HOST || 'https://portal.panelo.co/paneloresto',
--- src/config/sequalize.config.ts
+++ src/config/sequalize.config.ts
@@ -4,13 +4,12 @@ import { Product } from "../models/product.model";
 import { ProductCategory } from "../models/product-category.model";
 import { ProductImage } from "../models/product-image.model";
 import { ProductPrice } from "../models/product-price.model";
+import config from "./env.config";
+import {Dialect} from "sequelize";
 
-const sequelize = new Sequelize({
-    database: 'test_p',
-    dialect: 'mysql',
-    host: '127.0.0.1',
-    username: 'root',
-    password: '',
+const sequelize = new Sequelize( config.database.db, config.database.username, config.database.password, {
+    host: config.database.host,
+    dialect: config.database.driver as Dialect,
     models: [Category, Product, ProductCategory, ProductImage, ProductPrice],
 });
 
